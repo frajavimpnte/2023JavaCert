b92de1c109fb4edcb499bf89b4a897897104f8f6
Cristosuario28 <104657022+Cristosuario28@users.noreply.github.com>
2023-04-13 10:17:07 -0500

    Clase e08 commit inicial de mas clases. >Yanez Cristo ahora es colaborador.

--- E09_aunMasClases/src/e09_aunmasclases/E09_aunMasClases.java
+++ E09_aunMasClases/src/e09_aunmasclases/E09_aunMasClases.java
@@ -15,6 +15,7 @@ public class E09_aunMasClases {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Soy Yanez Cristo");
     }
     
 }
